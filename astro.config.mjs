73b5cc76820ada4137a83a85faba7bc837083cd2
Cameron McAteer <246350779+cameron-mcateer@users.noreply.github.com>
2026-05-13 14:47:30 -0700

    remove todo comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: set to the production URL (used by RSS, sitemap, canonical links).
   site: 'https://mcateer.dev',
   adapter: cloudflare(),
 });
